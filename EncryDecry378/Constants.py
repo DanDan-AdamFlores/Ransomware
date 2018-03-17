eb397644332a93e1771cfa8f53728dbe8af89b9f
Adam U Flores <watsaname1999@gmail.com>
2018-03-17 15:23:35 -0700

    Forgot Constants File

--- EncryDecry378/Constants.py
+++ EncryDecry378/Constants.py
@@ -2,6 +2,10 @@
 IV_KEY_LENGTH = 16
 #Defines the size of the encryption key length
 ENC_DEC_KEY_LENGTH = 32
+# Public Exponent - A Fermat Primes
+PUBLIC_EXPONENT = 65537
+# Key Size for RSA Encr
+RSA_KSZ = 4096
 #Index value for retrieving extensions from Json variable
 EXT = 'ext'
 #Index value for retrieving cipher from Json variable
